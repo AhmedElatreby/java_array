6bee745f7750a8b82c673eb8881abd26d321d63c
ahmedsport <ahmedsport@hotmail.com>
2022-07-25 22:02:13 +0100

    create an array

--- 01_01/begin/PracticeArrays/src/PracticeArrays.java
+++ 01_01/begin/PracticeArrays/src/PracticeArrays.java
@@ -1,6 +1,10 @@
 public class PracticeArrays {
 
     public static void main(String[] args) {
-        double[] lotteryNums;
+        double[] lotteryNums = {10, 20, 30, 40, 50}; // create an array
+        System.out.println(lotteryNums[3]);
+        System.out.println();
+        lotteryNums[3] = 44; // change a value in the array
+        System.out.println(lotteryNums[3]);
     }
 }
